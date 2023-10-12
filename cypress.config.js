af3ba25a5a197cf405865fe225f88c272337cb56
Mostafa Ayed Saad <mostafa.saad@flairstech.com>
2023-10-12 10:54:59 +0300

    cypres

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: 'https://www.programsbuzz.com/user/login',
     "specPattern": "cypress/e2e/**/*.{feature,features}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     
-       
+      require('cypress-mochawesome-reporter/plugin')(on);
         
   
       return require('./cypress/plugins/index.js')(on, config)
