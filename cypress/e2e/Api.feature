3296837641276c87d205e6a41aeaeb216be0e1cc
Mostafa Ayed Saad <mostafa.saad@flairstech.com>
2023-10-31 18:05:35 +0300

    31/10/2023

--- cypress/e2e/Api.feature
+++ cypress/e2e/Api.feature
@@ -5,6 +5,9 @@ Scenario:api get Scenario
 Given get request
 
 
-# @testkay=api2
-# Scenario:api post Scenario 
-# Given post request
+@testkay=api2
+Scenario:api post Scenario 
+Given users post request
+Then  posts post request
+Then API - PUT Request
+Then API - DELETE Request
--- cypress/e2e/Api/api.js
+++ cypress/e2e/Api/api.js
@@ -1,9 +1,15 @@
 
 import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps'
 import { Generate_app_id,} from '../../support/utlis/common.cy'
+import { faker } from '@faker-js/faker';
+import { describe } from 'mocha';
 
 
+let att1
+let att2
+let random=Generate_app_id(100000000,999999999)
 Given('get request',()=>{
+
      cy.request({
       method:'GET',
      
@@ -13,18 +19,20 @@ cy.get('@details').its('status').should('eq', 200)
 cy.get('@details').then((response) => {
     cy.log(JSON.stringify(response.body))
     cy.log(JSON.stringify(response.body.id))
+    
     cy.readFile("cypress/fixtures/users.json").then((profile) => {
      profile= response.body
      cy.writeFile('cypress/fixtures/users.json', profile)
+
  })
 })     
 })
-Given('post request',()=>{
-     var random=Generate_app_id(100000000,999999999)
+Given('users post request',()=>{
+     
 cy.log(random)
      cy.request({
           method: 'POST',
-   url: "https://gorest.co.in/public/v2/posts/",
+   url: "https://gorest.co.in/public/v2/users/",
      failOnStatusCode: false,
            'auth': {
                'bearer': "e2360bb230c2fc83af7451f8346d9fcdb699879e3a8f14462a2187f9d670675e"
@@ -32,20 +40,90 @@ cy.log(random)
      
           //Expecting the response status code to be 200
         
-          body: {'user':att2,
-          'user_id': random,
+        body: {'name':faker.internet.userName(),
+          'gender':'male',
           'email':faker.internet.email(),
-          'title':'This is a title',
-          'body': 'This is a message'} }).as('details')
+          'status':'active'} }).as('details')
           //Validate status code
           cy.get('@details').its('status').should('eq', 201)
           cy.get('@details').then((response) => {
               let res = response.body
-              let post_id = res.id
-              cy.log(post_id)
+             att1= res.id
+             att2= res.user
+          
           })
           cy.get('@details').then((response) => {
               cy.log(JSON.stringify(response.body))
           })
           
+})
+Then('posts post request',()=>{
+
+
+
+    cy.request({
+        method: 'POST',
+        url: "https://gorest.co.in/public/v2/posts/",
+        failOnStatusCode: false,
+        'auth': {
+            'bearer': "e2360bb230c2fc83af7451f8346d9fcdb699879e3a8f14462a2187f9d670675e"
+        },
+        body: {'user':att2,
+        'user_id': att1,
+        'email':faker.internet.email(),
+        'title':random,
+        'body': random}
+    }).as('details')
+    //Validate status code
+    cy.get('@details').its('status').should('eq', 201)
+    cy.get('@details').then((response) => {
+        let res = response.body
+        let post_id = res.id
+        cy.log(post_id)
+    })
+    cy.get('@details').then((response) => {
+        cy.log(JSON.stringify(response.body))
+    })
+       
+})
+Then('API - PUT Request',()=>{
+
+    cy.request({
+        method: 'PUT',
+        url:"https://gorest.co.in/public/v2/users/"+att1,
+        failOnStatusCode: false,
+        'auth': {
+            'bearer': "e2360bb230c2fc83af7451f8346d9fcdb699879e3a8f14462a2187f9d670675e"
+        },
+        body: {'name':faker.internet.userName(),
+        'gender':'female',
+        'email':faker.internet.email(),
+        'status':'active'}
+    }).as('details')
+    //Validate status code
+    cy.get('@details').its('status').should('eq', 200)
+    cy.get('@details').then((response) => {
+        cy.log(JSON.stringify(response.body))
+    })
+
+
+
+
+}) 
+Then('API - DELETE Request', () => {
+    cy.request({
+        method: 'DELETE',
+        url:"https://gorest.co.in/public/v2/users/"+att1,
+        failOnStatusCode: false,
+        'auth': {
+            'bearer': "e2360bb230c2fc83af7451f8346d9fcdb699879e3a8f14462a2187f9d670675e"
+        },
+    }).as('details')
+    //Validate response code
+    cy.get('@details').its('status').should('eq', 204)
+    cy.get('@details').then((response) => {
+        cy.log(JSON.stringify(response.body))
+    })
+   
+       
 })
